b795d9bb4e3eee519d74f0f561435f64301c5015
Osvaldo Júnior <osvaldojunior95@gmail.com>
2022-08-08 23:06:34 -0300

    instalado todos os packages e construido estrutura do projeto

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import router from "./routes/routes.js";
+import handlerError from "./exception/handlerError.js";
+import "./config/config.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+app.use(handlerError);
+
+export default app;
